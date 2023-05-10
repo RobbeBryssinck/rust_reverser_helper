050de625c9e4db6f13988d6195fb58dc60d37ebb
Robbe Bryssinck <robbebryssinck@gmail.com>
2023-05-10 17:40:05 +0200

    Test performance improvements

--- test_rust_reverser_helper.py
+++ test_rust_reverser_helper.py
@@ -25,18 +25,19 @@ class RustReverserTests(unittest.TestCase):
     def test_multiple_return_false_negatives(self):
         for function in self.symbols.functionSymbols:
             return_type_id = function.returnTypeId
-            for type in self.symbols.typeSymbols:
-                if type.id != return_type_id:
-                    continue
-
-                if type.length <= 8 or type.length > 16:
-                    continue
+            
+            if not return_type_id in self.types_by_id:
+                continue
+            type = self.types_by_id[return_type_id]
+            
+            if type.length <= 8 or type.length > 16:
+                continue
                 
-                address = self.base + function.virtualAddress
-                function_details = helpers.get_function_details(address)
+            address = self.base + function.virtualAddress
+            function_details = helpers.get_function_details(address)
 
-                with self.subTest(msg="{}: {}, {}".format(hex(address), function.id, function.name)):
-                    self.assertEqual(function_details.rettype.get_size(), 16)
+            with self.subTest(msg="{}: {}, {}".format(hex(address), function.id, function.name)):
+                self.assertEqual(function_details.rettype.get_size(), 16)
 
     def test_multiple_return_false_positives(self):
         for address in idautils.Functions():
@@ -47,16 +48,18 @@ class RustReverserTests(unittest.TestCase):
 
             virtual_address = address - self.base
 
-            for function in self.symbols.functionSymbols:
-                if function.virtualAddress != virtual_address:
-                    continue
+            if not virtual_address in self.functions_by_virtual_address:
+                continue
+            function = self.functions_by_virtual_address[virtual_address]
 
-                return_type_id = function.returnTypeId
-                for type in self.symbols.typeSymbols:
-                    if type.id != return_type_id:
-                        continue
-                    with self.subTest(msg="{}: {}, {}".format(hex(address), function.id, function.name)):
-                        self.assertTrue(type.length > 8 and type.length <= 16, "{}: {}".format(hex(address), function.id))
+            return_type_id = function.returnTypeId
+
+            if not return_type_id in self.types_by_id:
+                continue
+            type = self.types_by_id[return_type_id]
+
+            with self.subTest(msg="{}: {}, {}".format(hex(address), function.id, function.name)):
+                self.assertTrue(type.length > 8 and type.length <= 16, "{}: {}".format(hex(address), function.id))
 
 
 if __name__ == "__main__":
