3fac3eee01779552ccdc59e84d832ceae3963abe
Robbe Bryssinck <robbebryssinck@gmail.com>
2023-05-10 17:39:59 +0200

    Ida detect global (empty) strings

--- ida/rust_strings.py
+++ ida/rust_strings.py
@@ -11,6 +11,29 @@ def identify_rust_strings():
     for function_address in idautils.Functions():
         identify_rust_strings_in_function(function_address)
 
+def is_global_rust_string(address: int):
+    if not is_in_data_section(address):
+        return False
+
+    length: int = idc.get_qword(address + 8)
+
+    # Global Rust strings should have the string data right before the str structure.
+    # It is possible that the compiler injects padding for 8-byte alignment.
+    string_address: int = idc.get_qword(address)
+    padding: int = address - string_address - length
+    if padding < 0 or padding >= 8:
+        return False
+
+    for i in range(length):
+        if not chr(idc.get_wide_byte(address + i)).isascii():
+            return False
+
+    return True
+
+def is_global_rust_string_empty(address: int):
+    # Empty strings in Rust point to themselves.
+    return idc.get_qword(address) == address and idc.get_qword(address + 8) == 0
+
 def identify_rust_strings_in_function(function_address: str):
     instructions: List[int] = helpers.get_instructions_from_function(function_address)
 
@@ -24,9 +47,13 @@ def identify_rust_strings_in_function(function_address: str):
             continue
 
         if "off_" in idc.print_operand(instructions[i], 1):
+            if is_global_rust_string_empty():
+                set_string_name(source_address, "raEmpty")
+                continue
+
             string_length: int = idc.get_qword(source_address + 8)
             label: str = create_rust_string_label(idc.get_qword(source_address), string_length)
-            idc.set_name(source_address, label)
+            set_string_name(source_address, label)
             continue
 
 def is_in_data_section(address: int) -> bool:
@@ -44,6 +71,7 @@ def create_rust_string_label(address: int, length: int) -> str:
 
     return label
 
+# TODO: map that marks addresses that have been labeled already?
 def set_string_name(address: int, label: str) -> bool:
     if not label.isascii():
         return False
@@ -51,6 +79,7 @@ def set_string_name(address: int, label: str) -> bool:
     idc.set_name(address, label)
     return True
 
+# TODO: map that marks addresses that have been commented already?
 def set_string_comment(address: int, label: str) -> bool:
     if not label.isascii():
         return False
